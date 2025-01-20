ee6190a34129c5c2d18404c10cbdb9503753b644
Dcssa <danensaka13@google.com>
2025-01-20 09:46:35 +0700

    error import

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import * as THREE from 'three'
 import Lights from './Lights';
 import Loader from './Loader';
-import Phone from './IPhone.jsx';
+import Phone from './iPhone.jsx';
 import { Suspense } from "react";
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
